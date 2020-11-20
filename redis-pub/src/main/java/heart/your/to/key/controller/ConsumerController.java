b043c933d1c52cae2251cd9f624ef10a28cd85e0
lichenke <llw04758@gmail.com>
2020-11-20 15:21:51 +0800

    更新

--- redis-pub/src/main/java/heart/your/to/key/controller/ConsumerController.java
+++ redis-pub/src/main/java/heart/your/to/key/controller/ConsumerController.java
@@ -1,6 +1,8 @@
 package heart.your.to.key.controller;
 
 import com.its.ione.v3.cmdb.api.pojo.Ci;
+import com.its.ione.v3.cmdb.service.micro.CiRegisterTask.RegisterConfig;
+import com.its.ione.v3.core.jsonrpc.tools.JwtRpcClient;
 import com.its.ione.v3.micro.register.consumer.ConsumerUtils;
 import com.its.ione.v3.micro.register.consumer.pojo.Consumer;
 import com.its.ione.v3.micro.register.consumer.pojo.JWTCredential;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * @author LiChenke
@@ -46,5 +49,35 @@ public class ConsumerController {
     public String registerConsumer(@RequestBody Consumer consumer) {
         ConsumerUtils.registerConsumer(consumer);
         return consumer.getUsername() + "注册成功!";
+    }
+
+    @PostMapping("/consumer/deployJWT")
+    public String deployJWT(String usernameOrId) {
+        ConsumerUtils.deployJWTCredential(usernameOrId);
+        return "成功";
+    }
+
+    @PostMapping("/consumer/deployJWT2")
+    public String deployJWT(String usernameOrId, String key, String secret) {
+        ConsumerUtils.deployJWTCredential(usernameOrId, key, secret);
+        return "成功";
+    }
+
+    @GetMapping("/consumer/test")
+    public String test() {
+        Supplier<String> supplier = ConsumerUtils.buildJwtSupplier("user");
+        JwtRpcClient client = new JwtRpcClient(supplier, "http://localhost:8000/relax/rpc");
+        Ci ci = client.safeCall(Ci.class, "/v2/login/login", "admin", "admin1", false);
+        return JSON.toJSONString(ci);
+    }
+
+    @PostMapping("/home/register/cis")
+    public String registerCis(@RequestBody RegisterConfig config) {
+        StringBuilder sb = new StringBuilder("ci info:");
+        List<Ci> cis = config.getCis();
+        cis.forEach(ci -> sb.append(ci.getName()).append(" "));
+        sb.append(config.getConfig().get("type")).append(" ").append(config.getConfig().get("application"));
+        log.info(sb.toString());
+        return sb.toString();
     }
 }
